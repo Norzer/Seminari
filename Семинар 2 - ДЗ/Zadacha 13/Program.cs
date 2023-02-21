185802d0b2d9b727bbddac981a7b25a23c8dd72b
Kirill Klimentov <norzernorzer@gmail.com>
2023-02-21 19:30:15 +0300

    Zadacha13

---
+++ Семинар 2 - ДЗ/Zadacha 13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("3 number : " + anyNumberText[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+}
