4e8aa586f4839a5c0c1901c26306e8525574eb8b
Kirill Klimentov <norzernorzer@gmail.com>
2023-02-21 18:52:08 +0300

    Zadacha 6

---
+++ Семинар 1 - ДЗ/Zadacha 6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0){
+    Console.WriteLine("Yes");
+}
+else{
+    Console.WriteLine("No");
+}
