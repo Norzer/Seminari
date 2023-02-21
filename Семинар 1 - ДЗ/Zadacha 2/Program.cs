51c6d22bbd82ec3c2275ad3686949df82deaf34f
Kirill Klimentov <norzernorzer@gmail.com>
2023-02-21 18:31:34 +0300

    Zadacha1

--- Семинар 1 - ДЗ/Zadacha 2/Program.cs
+++ Семинар 1 - ДЗ/Zadacha 2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) {
+    Console.WriteLine($"max = {number1}");
+}
+else {
+    Console.WriteLine($"max = {number2}");
+}
