694dd30586abef78cd1b89cbc76dba656e675abe
Kirill Klimentov <norzernorzer@gmail.com>
2023-02-21 19:00:37 +0300

    Zadacha 8

---
+++ Семинар 1 - ДЗ/Zadacha 8.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int StartNum = 2;
+
+if(num > 1)
+{
+    while(StartNum <= num)
+    {
+        Console.Write(StartNum + " ");
+        StartNum = StartNum + 2;
+    }
+}
