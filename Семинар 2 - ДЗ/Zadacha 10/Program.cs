8562e35a36f1369bca81fa7c65340e9d30d3e4b3
Kirill Klimentov <norzernorzer@gmail.com>
2023-02-21 19:18:40 +0300

    Pere

---
+++ Семинар 2 - ДЗ/Zadacha 10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите 3 значное целочисленное число ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine(" Число:" +stringNumber[1]);
