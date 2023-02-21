31ece98caccd87a0a6a87522efa6ce4929350c21
Kirill Klimentov <norzernorzer@gmail.com>
2023-02-21 18:42:35 +0300

    Zadacha4

---
+++ Семинар 1 - ДЗ/Zadacha 4/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("Enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 3 number");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (number1 > number2) {
+    max = number1;
+}
+else {
+max = number2;
+}
+
+if (max < number3) {
+    max = number3;
+}
+
+Console.WriteLine($"max = {max}");
