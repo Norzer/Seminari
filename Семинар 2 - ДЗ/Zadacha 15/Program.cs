c8365b8343bef9764379e13ffdd53e02d76ab78c
Kirill Klimentov <norzernorzer@gmail.com>
2023-02-21 19:35:05 +0300

    Zadacha15

---
+++ Семинар 2 - ДЗ/Zadacha 15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter number (1,2,3,4,5,6,7) ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+    Console.WriteLine("Yes");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("Eror");
+  }
+  else Console.WriteLine("No");
+}
+CheckingTheDayOfTheWeek(dayNumber);
