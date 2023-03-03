c4a8d9698ae0443f9caf3bdfa2bacb1180a35e26
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-03 17:12:30 +0300

    version 1

--- Семинар 5 - дз/Задача 36/Program.cs
+++ Семинар 5 - дз/Задача 36/Program.cs
@@ -7,10 +7,10 @@ int sum = 0;
 
 void FillArray(int[] array)
 {
-  for (int i = 1; i < n; i++)
+  for (int i = 0; i < n; i++)
   {
     array[i] = r.Next(100);
-    Console.WriteLine(array[i] + " ");
+    //Console.WriteLine(array[i] + " ");
   }
 }
 
---
+++ Семинар 5 - дз/Задача 38/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите длинну массива");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+Random r = new Random();
+double razn = 0;
+double max = 0;
+double min = 1111111110;
+
+
+
+
+void FillArray(double[] array)
+{
+  for (int i = 0; i < n; i++)
+  {
+    array[i] = r.Next(100) + r.NextDouble();
+    //Console.WriteLine(array[i] + " ");
+  }
+}
+
+void Raznica(double[] array)
+{
+  for (int i = 0; i < n; i++)
+  {
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+  }
+
+razn = max - min;
+Console.WriteLine(razn);
+}
+
+FillArray(array);
+Raznica(array);
