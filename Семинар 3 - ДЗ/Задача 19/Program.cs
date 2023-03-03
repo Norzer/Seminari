4a2f922f2ceccd821b425ab9d607804bd03a0f1c
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-03 14:55:20 +0300

    1

--- Семинар 3 - ДЗ/Задача 19/Program.cs
+++ Семинар 3 - ДЗ/Задача 19/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите 5-значное число: ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine("Да.");
+  }
+  else Console.WriteLine("Нет");
+}
+
+if (number.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введенное число не 5-значное");
