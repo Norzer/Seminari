fcdf75c8fd3096008f11681bd430c852be4b5735
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-03 16:32:53 +0300

    version 1

---
+++ Семинар 5 - дз/Задача 36/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите длинну массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Random r = new Random();
+int sum = 0;
+
+
+void FillArray(int[] array)
+{
+  for (int i = 1; i < n; i++)
+  {
+    array[i] = r.Next(100);
+    Console.WriteLine(array[i] + " ");
+  }
+}
+
+void SumChet( int[] array) 
+{
+    for (int i = 1; i < n; i++)
+    {
+        if ( i % 2 != 0)
+        {
+           sum = sum + array[i];
+        }
+        else
+        {
+           continue;
+        };
+    }
+ Console.WriteLine($"Сумма: ",sum);
+}
+
+FillArray(array);
+SumChet(array);
