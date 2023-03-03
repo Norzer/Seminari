271a4d835b957f76ed0a56eeb95eeb289d4c78f9
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-03 14:50:17 +0300

    1.01

---
+++ Seminar4/Zadacha28/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number n ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[8];
+int sluchainaya_velichina = 0;
+Random rand = new Random();
+for (int i = 0; i < 8; i++)
+{
+                array[i] = sluchainaya_velichina.Next(0, 2); 
+                Console.Write(array[i]);
+}
