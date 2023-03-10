f39ee08fd67af991b273a40444a12608f5189157
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-10 15:03:35 +0300

    Посмотрел семинар 3

---
+++ Семинар 3 ДЗ - ИЗМЕНЕННОЕ/Задача 23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число до которого будут указаны кубы ");
+int n = Convert.ToInt32(Console.ReadLine());
+Cube(n);
+
+void Cube(int numb)
+{
+    int i = 1;
+    while (i <= numb)
+    {
+        Console.WriteLine($"{i} - {i * i * i}");
+        i++;
+    }
+}
