2eb25375a9c64487e3b1c47daec541504ef7ca16
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-10 17:36:05 +0300

    Что смог через костыли

--- Семинар 3 ДЗ - ИЗМЕНЕННОЕ/Задача 19/Program.cs
+++ Семинар 3 ДЗ - ИЗМЕНЕННОЕ/Задача 19/Program.cs
@@ -8,20 +8,16 @@ if (num1.Length != 5)
     Console.WriteLine("EROR!!!!!!!!!!!!!!!!");
 }
 
-//Console.WriteLine("Ваше число в обратном виде");
 int j = 0;
 for (int i = num1.Length - 1; i > -1; --i, j++)
     {
-        //Console.Write((num1[i]));
+        
         num2[j] = num1[i];
     }           
 numb2 = new string(num2);
-//Console.Write(num2);
+
 int a = 0;
-//int number1 = Convert.ToInt32(num1);
-//int number2 = int.Parse(num2.ToString());
 a = (String.Compare(numb2, num1));
-//Console.WriteLine(a);
 if (a == 0)
 {
     Console.WriteLine("Да");
@@ -30,13 +26,3 @@ else
 {
     Console.WriteLine("Нет");
 }
-//Console.WriteLine(numb2.Equals(num1));
-
-/*if (number2 == number1)
-{
-    Console.WriteLine("Да");
-}
-else 
-{
-    Console.WriteLine("Нет");
-}*/
