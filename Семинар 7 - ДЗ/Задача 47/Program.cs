1ba771bbcfe6bd085b099c75196ecba909eba560
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-10 18:53:18 +0300

    version1

---
+++ Семинар 7 - ДЗ/Задача 47/Program.cs
@@ -0,0 +1,40 @@
+﻿double[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+  double[,] matrix = new double[rows, columns]; // 0 , 1
+  Random r = new Random();
+
+  for (int i = 0; i < matrix.GetLength(0); i++) // rows
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++) // columns
+    {
+      matrix[i, j] = r.Next(min, max + 1) + r.NextDouble() ;//
+    }
+  }
+  return matrix;
+}
+
+
+void PrintMatrix(double[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+        if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5}, ");
+        else Console.Write($"{matrix[i, j], 5}");
+      }
+    Console.WriteLine("|");
+  }
+}
+Console.WriteLine("Размерность Матрицы: ");
+Console.Write("Введите количество строк = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Минимальный Диапозон заполнения числа = ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Максимальный Диапозон заполнения числа = ");
+int d = Convert.ToInt32(Console.ReadLine());
+double[,] matr = CreateMatrixRndInt(a, b, c, d);
+PrintMatrix(matr);
