e02f9910a967d03d7d103c3033b53a2ada5c471d
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-12 19:08:03 +0300

    Ver1

---
+++ Семинар 4 - ДЗ/Задача 27/Program.cs
@@ -0,0 +1,19 @@
+﻿int Sum(int num)
+{
+    int s = 0;
+    int k = 0;
+    while (num > 0)
+    {
+        k = num % 10;
+        s += k; 
+        num = num / 10;
+    }
+    return s;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(number));
+
+
+
