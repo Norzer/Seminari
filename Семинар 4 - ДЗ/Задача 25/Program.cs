baa7c65f17c216221a94c9f60382db851a0a1145
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-12 18:28:07 +0300

    Ver1

---
+++ Семинар 4 - ДЗ/Задача 25/Program.cs
@@ -0,0 +1,20 @@
+﻿int Pow(int a , int b) 
+{
+    int c = 1; 
+    for ( int i = 0; i < b ; i++)
+    {
+        c *= a;
+        //Console.WriteLine("----" + c);
+    }
+    return c;
+}
+
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Степень");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Pow(num1, num2));
+
+
