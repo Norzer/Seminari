e9535080b21b5eeb21118754ca3c0f0bbf8866ec
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-12 19:22:43 +0300

    Ver1

---
+++ Семинар 4 - ДЗ/Задача 29/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] array = new int[8];
+Random r = new Random();
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = r.Next(100);
+    }
+}
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++ )
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+FillArray(array);
+Print(array);
+
