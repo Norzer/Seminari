ff197f148043395b95f1f6b61d46a61bc978f459
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-13 04:28:35 +0300

    Ver1.0

---
+++ Семинар 8 - ДЗ/Задача 60/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth]; // 0 , 1, 2
+    Random rnd = new Random();
+    int x = 10;
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // columns
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                //matrix[i, j, k] = rnd.Next(min, max + 1);
+                matrix[i, j, k] = x;
+                x++;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.WriteLine($"{matrix[i,j,k]}({i},{j},{k})");
+            }
+        }
+     
+    }
+}
+
+Console.WriteLine("Размерность Матриц : ");
+Console.Write("Введите количество строк = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите глубину = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int[,,] matr = CreateMatrixRndInt(a, b, c, 10, 99);
+PrintMatrix(matr);
+
