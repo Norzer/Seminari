615b5d10a6742fe7b5e0d5d2ef5f5d0cbf012160
Kirill Klimentov <norzernorzer@gmail.com>
2023-03-17 15:40:38 +0300

    version1

---
+++ Семинар 9 - ДЗ/Задача 64/Program.cs
@@ -0,0 +1,14 @@
+﻿//добавить проверку
+
+Console.WriteLine("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0 )  NaturaleNumber(number);
+else Console.WriteLine("ОШИБКА") ;
+
+void NaturaleNumber(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num}"); //- хвостовая рекурсия
+    NaturaleNumber(num - 1);
+}
